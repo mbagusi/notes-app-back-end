57ca615923aff3b19f4fa46d513253a12c9cf700
mbagusi <bagusinsan21@gmail.com>
2022-05-17 19:01:10 +0700

    fix bug host value

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 
-const init = async () => {
-  const server = Hapi.server({
+const server = Hapi.server({
     port: 5000,
-    host: 'localhost',
-    
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
   });
 
   server.route(routes);
@@ -13,6 +16,5 @@ const init = async () => {
   await server.start();
   // eslint-disable-next-line no-console
   console.log(`Server berjalan pada ${server.info.uri}`);
-};
 
 init();
